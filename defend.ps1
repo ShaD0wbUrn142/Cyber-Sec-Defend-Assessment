71cb4ee0f06fdb0d2462757326665522355a3430
~Nekopat <57177658+Happypat900@users.noreply.github.com>
2023-08-27 16:24:05 +1000

    Update defend.ps1

--- defend.ps1
+++ defend.ps1
@@ -1,3 +1,10 @@
+$arg=$args[0]
+if($arg){
+	write-host $arg
+}else{
+	write-host "No args given"
+}
+
 function testDNSBlock {
     $nic = Get-NetAdapter | Where-Object { $_.Name -eq 'Ethernet 3' }
     if ($nic) {
@@ -12,4 +19,42 @@ function testDNSBlock {
     }
 }
 
-testDNSBlock
+function enableDoH {
+	Set-ItemProperty -Path "HKLM:SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" -Name "EnableAutoDoH" -Value "2"
+	echo "rebooting in 5 seconds, press ctrl+c to cancel"
+	Start-Sleep -Milliseconds 5000
+	Restart-Computer
+}
+
+function setupQuadDoH {
+
+}
+
+function resetDoH {
+	$networkInterface = Get-NetAdapter | Where-Object { $_.InterfaceAlias -eq 'Ethernet 3' }
+	Set-DnsClientServerAddress -InterfaceIndex $adapterIndex -ServerAddresses "10.0.2.3"
+}
+
+switch ($arg) {
+    "DoH-test" {
+        # Call the testDNSBlock function
+        testDNSBlock
+    }
+    "DoH-enable" {
+        # Call the enableDoH function
+        enableDoH
+    }
+    "setupQuadDoH" {
+        # Call the setupQuadDoH function
+        setupQuadDoH
+    }
+	"resetDoH" {
+        # Call the resetDoH function
+        resetDoH
+    }
+    default {
+        Write-Output "Invalid argument provided. Use: DoH-test, DoH-enable, setupQuadDoH, resetDoH."
+    }
+}
+
+#testDNSBlock
